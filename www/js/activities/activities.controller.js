c3086326c72562902cc54d180f3793fda109cca8
ienemien <Tompoes!85>
2015-10-08 12:14:42 +0200

    Added delete functionality

--- www/js/activities/activities.controller.js
+++ www/js/activities/activities.controller.js
@@ -5,8 +5,36 @@
     .module('timetomove')
     .controller('ActivitiesCtrl', ActivitiesController);
 
-  function ActivitiesController(activities) {
+  function ActivitiesController(activities, $ionicPopup, $state, ActivitiesFactory) {
     var vm = this;
     vm.activities = activities;
+    vm.showActivity = showActivity;
+    vm.deleteActivity = deleteActivity;
+    vm.confirmDelete = confirmDelete;
+
+    function showActivity(id) {
+      alert("in show activity");
+      return "/activities/" + id;
+    }
+
+    function deleteActivity(id) {
+      ActivitiesFactory.delete({id: id});
+      $state.reload();
+    }
+
+    function confirmDelete(activity) {
+      var confirmPopup = $ionicPopup.confirm({
+        title: 'Delete Activity',
+        template: 'Are you sure you want to delete \'' + activity.type + '\'?'
+      });
+      confirmPopup.then(function(res) {
+        if(res) {
+          console.log('Delete activity');
+          deleteActivity(activity.id);
+        } else {
+          console.log('You are not sure');
+        }
+      });
+    }
   }
 })();
